0f35b28480d5e7e51094b816c7900d3cbbb3bcab
rafael <rafael.eustapinto@gmail.com>
2023-04-27 14:05:28 -0300

    Divisão dos conteúdos nazismo e navegação com botôes.

---
+++ js/nazism_page_buttons.js
@@ -0,0 +1,71 @@
+
+var totas_as_divisoes = ["origem", "ascensao", "dinamicas", "fim"];
+var todos_os_botoes = ["botao_origem", "botao_ascensao", "botao_dinamicas", "botao_fim"]
+var divisao_atual = undefined;
+var cor_sessao_ativa = 'blue';
+var cor_sessao_off = 'red';
+
+function hide(element_id){
+    document.getElementById(element_id).style.display = 'none';
+}
+
+function updateButtonCollor(element_id){
+    if(element_id === divisao_atual){
+        document.getElementById('botao_' + element_id).style.background = cor_sessao_ativa
+    }else{
+        document.getElementById('botao_' + element_id).style.background = cor_sessao_off
+    }
+
+}
+
+function atualizarCorDosBotoes(){
+    totas_as_divisoes.forEach(updateButtonCollor)
+}
+
+function mudarDivisaoDaPaginaNazismo(element_id){
+    divisao_atual = element_id
+    totas_as_divisoes.forEach(hide);
+    document.getElementById(element_id).style.display = 'block';
+    atualizarCorDosBotoes()
+    
+}
+
+function avançar(){
+    if(divisao_atual === 'origem'){
+        mudarDivisaoDaPaginaNazismo('ascensao')
+    }else if(divisao_atual === 'ascensao'){
+        mudarDivisaoDaPaginaNazismo('dinamicas')
+    }else if(divisao_atual === 'dinamicas'){
+        mudarDivisaoDaPaginaNazismo('fim')
+    }
+    controlarBotoesVoltarEAvançar()
+
+}
+
+function voltar(){
+    if(divisao_atual === 'ascensao'){
+        mudarDivisaoDaPaginaNazismo('origem')
+    }else if(divisao_atual === 'dinamicas'){
+        mudarDivisaoDaPaginaNazismo('ascensao')
+    }else if(divisao_atual === 'fim'){
+        mudarDivisaoDaPaginaNazismo('dinamicas')
+    }
+    controlarBotoesVoltarEAvançar()
+}
+
+
+function controlarBotoesVoltarEAvançar(){
+    if(divisao_atual === 'origem'){
+        document.getElementById('voltar').style.display = 'none'
+        document.getElementById('avançar').style.display = 'block'
+    }else if(divisao_atual === 'ascensao'){
+        document.getElementById('voltar').style.display = 'block'
+        document.getElementById('avançar').style.display = 'block'
+    }else if(divisao_atual === 'dinamicas'){
+        document.getElementById('voltar').style.display = 'block'
+        document.getElementById('avançar').style.display = 'block'
+    }else if(divisao_atual === 'fim'){
+        document.getElementById('voltar').style.display = 'block'
+        document.getElementById('avançar').style.display = 'none'
+    }
+}
